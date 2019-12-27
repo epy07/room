9c1791c0461490bd23994db06a380e3124ea9c6a
Jeffrey <adawagEpy@gmail.com>
2019-12-27 22:37:43 +0800

    Initial commit

---
+++ app/src/main/java/learn/android/mynotes/MainActivity.kt
@@ -0,0 +1,12 @@
+package learn.android.mynotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
